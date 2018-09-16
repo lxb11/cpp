e87f4c391c0a07aa12fdaf3ea33e42428b3d0f1a
lxb11 <623417725@qq.com>
2018-09-16 21:25:25 +0800

    learning

--- practice/cast/reinterpret_cast.cc
+++ practice/cast/reinterpret_cast.cc
@@ -1,70 +1,87 @@
-#define  _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
-
 using namespace std;
 
-//动物的抽象类
+
 class Animal
 {
 public:
-	virtual void cry() = 0;// 纯虚函数
-	virtual void doHome() = 0;// 纯虚函数
+    virtual void cry() = 0;// 纯虚函数
+    virtual void doHome() = 0;// 纯虚函数
+
 };
 
 class Dog :public Animal {
 public:
-	Dog(int a){
-		this->a = a;
-	}
-	virtual void cry()  {
-		cout << "汪汪~~" << endl;
-	}
+    Dog()
+    {
+        this->a = 0;
+    }
 
-	void doHome() {
-		cout << "看家" << endl;
-	}
-	int a;
+    Dog(int a){
+        this->a = a;
+
+    }
+    virtual void cry()  {
+        cout << "wangwang ~~" << endl;
+
+    }
+
+    void doHome() {
+        cout << "watch home ---" << endl;
+
+    }
+    int a;
+
 };
 
 class Cat :public Animal {
 public:
-	virtual void cry() {
-		cout << "喵喵~~" << endl;
-	}
-	void doHome() {
-		cout << "抓老鼠" << endl;
-	}
+    virtual void cry() {
+        cout << "miaomiao~~" << endl;
+
+    }
+    void doHome() {
+        cout << "seize rat----" << endl;
+
+    }
+
 };
 
 
 class Tree
 {
 public:
-	Tree(){
-		age = 10;
-	}
-	virtual void printT() {
-		cout << "我是大叔" << endl;
-	}
-	int age1;
-	int age; //在类的内存内部
+    Tree(){
+        age = 10;
+
+    }
+    virtual void printT() {
+        cout << "i am a tree" << endl;
+
+    }
+    int age1;
+    int age; //在类的内存内部
+
 };
 
 int main(void)
 {
-	Animal *animal = new Dog(10);
-   animal->cry();
+    Animal *animal = new Dog(100);
+    animal->cry();
 
 
-	Tree* tree = reinterpret_cast<Tree*>(animal); //C++中 真正的强制转换
-	if (tree != NULL) {
-		cout << "转换成功" << endl;
-		cout << tree->age << endl;
-		tree->printT(); //vptr
-	}
-	else {
-		cout << "转换失败" << endl;
-	}
-	
-	return 0;
+    Tree* tree = reinterpret_cast<Tree*>(animal); //C++中 真正的强制转换
+    if (tree != NULL) {
+        cout << "SUCC" << endl;
+        cout << tree->age << endl;
+        tree->printT(); //vptr
+
+    }
+    else {
+        cout << "fail" << endl;
+
+    }
+
+    return 0;
+
 }
