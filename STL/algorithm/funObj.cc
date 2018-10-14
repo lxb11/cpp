4331cdf4b48b9e22516439c6698f9d90b6239c58
lxb11 <623417725@qq.com>
2018-10-14 16:29:47 +0800

    learning

--- STL/algorithm/funObj.cc
+++ STL/algorithm/funObj.cc
@@ -50,14 +50,10 @@ struct mycompare2:public unary_function<int, bool>
 
 };
 
-class print1
+void print1(int v)
 {
-public:
-    void operator()(int v)
-    {
-        cout << v << "  ";
-    }
-};
+    cout << v << "  ";
+}
 
 void test2()
 {
@@ -90,14 +86,92 @@ void test2()
 
     //sort算法
     sort(vec.begin(), vec.end(), greater<int>());
-    for_each(vec.begin(), vec.end(), print1());
+    for_each(vec.begin(), vec.end(), print1);
     cout << endl;
 
-    sort(vec.begin(), vec.end(), not2(greater<int>()));
-    for_each(vec.begin(), vec.end(), print1());
+    sort(vec.begin(), vec.end(), not2(greater<int>()));//取反适配器
+    for_each(vec.begin(), vec.end(), print1);
     cout << endl;
 }
 
+
+//ptr_func 普通函数适配器
+//对普通函数进行绑定参数
+//1 需要将普通函数转化为函数对象 ptr_fun
+//2 可以用bind适配器了
+void print2(int v, int v2)
+{
+    if(v > v2)
+        cout << v << "  ";
+}
+
+void test3()
+{
+    vector<int> vec;
+    for(int i = 0; i < 10; ++i)
+    {
+        vec.push_back(i);
+    }
+    for_each(vec.begin(), vec.end(), bind2nd(ptr_fun(print2), 5));
+    cout << endl;
+}
+
+//mem_fun mem_fun_ref
+//
+
+class Teacher
+{
+public:
+    Teacher(int id, int age):_id(id), _age(age){}
+    int _id;
+    int _age;
+
+    void printTeacher() const
+    {
+        cout << "id:" << this->_id <<  "  age:" << this->_age << endl;
+    }
+};
+
+class print3
+{
+public:
+    void operator()(Teacher t)
+    {
+        cout << "id:" << t._id << "  age:" << t._age << endl;
+    }
+};
+
+void test4()
+{
+    vector<Teacher> v;
+    Teacher t1(1, 20), t2(2, 30), t3(3, 40);
+    v.push_back(t1);
+    v.push_back(t2);
+    v.push_back(t3);
+
+    //函数对象方式
+    for_each(v.begin(), v.end(), print3());
+    cout << endl;
+    
+    //用类的成员函数来打印对象
+    for_each(v.begin(), v.end(), mem_fun_ref(&Teacher::printTeacher));
+    cout << endl;
+
+    //如果你的容器中放的是类对象实体，用mem_fun_ref
+    //如果你的容器放的是类对象指针，用，mem_fun
+    
+    vector<Teacher*> v2;
+    Teacher* tt1 = new Teacher(4, 22);
+    Teacher* tt2 = new Teacher(5, 33);
+    Teacher* tt3 = new Teacher(6, 44);
+
+    v2.push_back(tt1);
+    v2.push_back(tt2);
+    v2.push_back(tt3);
+
+    for_each(v2.begin(), v2.end(), mem_fun(&Teacher::printTeacher));
+    cout << endl;
+}
 
 int main()
 {
@@ -105,6 +179,10 @@ int main()
     test1();
     cout << "----------test2-------------" << endl;
     test2();
+    cout << "----------test3-------------" << endl;
+    test3();
+    cout << "----------test4-------------" << endl;
+    test4();
     return 0;
 }
 
