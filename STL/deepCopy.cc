a9eebd212e96ff5ec720009ce8c791c4b4e38e24
lxb11 <623417725@qq.com>
2018-10-14 10:37:21 +0800

    learning

---
+++ STL/deepCopy.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+
+class Teacher
+{
+public:
+    Teacher(char* name, int age)
+    {
+        int len = strlen(name);
+        this->name = new char[len + 1];//在堆分配内存
+        strcpy(this->name, name);
+
+        this->age = age;
+    }
+
+    ~Teacher()
+    {
+        if(this->name != NULL)
+        {
+            delete[] this->name;
+        }
+        this->age = 0;
+    }
+
+    char* name;
+    int age;
+
+};
+
+
+//测试函数 深拷贝和浅拷贝
+void test1()
+{
+    Teacher t1("chris", 20);
+    vector<Teacher> vec;
+    vec.push_back(t1);
+
+
+}
+
+int main()
+{
+    cout << "---------test1-----------" << endl;
+    test1();
+    return 0;
+}
+
