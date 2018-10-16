5b67ca519b90fb36fd81f0f29078beb2bcb9c6fd
lxb11 <623417725@qq.com>
2018-10-16 15:45:28 +0800

    learning

---
+++ STL/algorithm/copyReplace.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+//copy
+
+void print(int v)
+{
+    cout << v << " ";
+}
+
+void test01()
+{
+    vector<int> v1, v2;
+    for(int i = 0; i < 10; ++i)
+    {
+        v1.push_back(i);
+    }
+
+    v2.resize(v1.size());
+    copy(v1.begin(), v1.end(), v2.begin());
+    for_each(v2.begin(), v2.end(), print);
+    cout << endl;
+}
+
+
+//replace
+struct mycompare02
+{
+    bool operator()(int v)
+    {
+        return v > 5;
+    }
+};
+void test02()
+{
+    vector<int> v1, v2;
+    for(int i = 0; i < 10; ++i)
+    {
+        v1.push_back(i);
+    }
+
+    for_each(v1.begin(), v1.end(), print);
+    cout << endl;
+    replace(v1.begin(), v1.end(), 6, 100);
+    for_each(v1.begin(), v1.end(), print);
+    cout << endl;
+
+    //replace_if
+    replace_if(v1.begin(), v1.end(), mycompare02(), 50);
+    for_each(v1.begin(), v1.end(), print);
+    cout << endl;
+
+    //swap
+    for(int i = 0; i < 10; ++i)
+    {
+        v2.push_back(i);
+    }
+
+    cout << "交换前：" << endl;
+    for_each(v1.begin(), v1.end(), print);
+    cout << endl;
+    for_each(v2.begin(), v2.end(), print);
+    cout << endl;
+
+    swap(v1, v2);
+
+    cout << "交换后：" << endl;
+    for_each(v1.begin(), v1.end(), print);
+    cout << endl;
+    for_each(v2.begin(), v2.end(), print);
+    cout << endl;
+}
+
+int main()
+{
+    cout << "-----------test01------------" << endl;
+    test01();
+    cout << "-----------test02------------" << endl;
+    test02();
+    return 0;
+}
+
